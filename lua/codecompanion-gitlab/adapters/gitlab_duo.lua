fd7727b74a6ecd016f01c0d395270697dc9604fe
Kraust <secretdragoon@gmail.com>
2025-08-16 19:57:09 -0400

    URLs should not be escaped with backticks.

--- lua/codecompanion-gitlab/adapters/gitlab_duo.lua
+++ lua/codecompanion-gitlab/adapters/gitlab_duo.lua
@@ -79,7 +79,8 @@ Your Response Must:
 2. Must be a serialized JSON-formatted OpenAI Response which has been minifiled before serialization.
 3. The text requested by the propmt must be serialized as a string and inserted into the JSON-formatted OpenAI response.
 If you're requested to return a JSON object:
-- You should serialize it in as a string represented as %s in the following format:
+1. URLs should not be escaped with backticks.
+2. You should serialize it in as a string represented as %s in the following format:
 ```json
 {
     "id": "chatcmpl-codecompanion-023",
