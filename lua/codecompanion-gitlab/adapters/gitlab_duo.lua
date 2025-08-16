751bc00a4292e56310c20989676b159955e9ec5e
Kraust <secretdragoon@gmail.com>
2025-08-16 18:52:29 -0400

    idk

--- lua/codecompanion-gitlab/adapters/gitlab_duo.lua
+++ lua/codecompanion-gitlab/adapters/gitlab_duo.lua
@@ -83,7 +83,7 @@ Your Response Must:
 1. Be wrapped in *** Begin Response / *** End Response markers"
 2. Must be a minifiled JSON-formatted OpenAI Response.
 3. The text requested by the propmt must be serialized as a string and inserted into the JSON-formatted OpenAI response.
-3. The text requested by the propmpt should not try and escape dobule quoted strings with backticks.
+4. Instances of "` should be replaced by ".
 ]]
             }
             table.insert(messages, 1, message)
